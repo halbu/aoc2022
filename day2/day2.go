4631006c420fe020df5ab62bcacf7858f891d533
halbu <fstream.c@gmail.com>
2022-12-06 23:12:43 +0000

    Day 2 solutions

---
+++ day2/day2.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"aoc2022/utils"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var data = utils.GetData("./day2/day2-input")
+
+	// Let's hardcode the outcomes. Seems simplest
+	outcomes := map[string]int{
+		"A X": 4,
+		"A Y": 8,
+		"A Z": 3,
+		"B X": 1,
+		"B Y": 5,
+		"B Z": 9,
+		"C X": 7,
+		"C Y": 2,
+		"C Z": 6,
+	}
+
+	var points int = 0
+	for _, e := range data {
+		fmt.Println(e)
+		var result int = outcomes[e]
+		points += result
+	}
+
+	fmt.Print("Day 2 Part 1 solution: " + strconv.Itoa(points))
+
+	// And let's hardcode the outcomes again
+	newStrategyOutcomes := map[string]int{
+		"A X": 3,
+		"A Y": 4,
+		"A Z": 8,
+		"B X": 1,
+		"B Y": 5,
+		"B Z": 9,
+		"C X": 2,
+		"C Y": 6,
+		"C Z": 7,
+	}
+
+	var newStrategyPoints int = 0
+	for _, e := range data {
+		fmt.Println(e)
+		var result int = newStrategyOutcomes[e]
+		newStrategyPoints += result
+	}
+
+	fmt.Print("Day 2 Part 2 solution: " + strconv.Itoa(newStrategyPoints))
+}
