636c0d7a38b5df9ec2f5b5daa4062ddb15dbb42a
halbu <fstream.c@gmail.com>
2022-12-07 23:11:59 +0000

    Day 7 solutions

---
+++ day7/day7.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"aoc2022/utils"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+type Node struct {
+	parent *Node
+	files  []int
+	dirs   []*Node
+}
+
+var dirList = []int{}
+
+func findAllDirsWithCondition(n *Node, val int, cond string) int {
+	var tot = 0
+	for _, e := range n.files {
+		tot += e
+	}
+
+	for _, e := range n.dirs {
+		tot += findAllDirsWithCondition(e, val, cond)
+	}
+
+	if (cond == "max" && tot <= val) || (cond == "min" && tot >= val) || cond == "none" {
+		dirList = append(dirList, tot) // Push any dir that meets the conditions into `dirList`
+	}
+	return tot // Return the total filesize of this directory and all its subdirectories
+}
+
+func main() {
+	var data = utils.GetData("./day7/day7-input")
+
+	var currentDir *Node
+	var root = Node{parent: nil, files: []int{}, dirs: []*Node{}}
+	currentDir = &root
+
+	// Parse all directories and files and shove them into our crude tree structure
+	for _, e := range data {
+		if utils.StartsWith(e, "$ cd ") {
+			if utils.Remainder(e, "$ cd ") == ".." {
+				currentDir = currentDir.parent
+			} else {
+				var newNode = Node{parent: currentDir, files: []int{}, dirs: []*Node{}}
+				currentDir.dirs = append(currentDir.dirs, &newNode)
+				currentDir = &newNode
+			}
+		}
+
+		if unicode.IsDigit(rune(e[0])) {
+			var filesize int = utils.StringToInt(strings.Split(e, " ")[0])
+			currentDir.files = append(currentDir.files, filesize)
+		}
+	}
+
+	dirList = []int{}
+	findAllDirsWithCondition(&root, 100000, "max")
+	fmt.Println("Day 7 Part 1 solution: " + strconv.Itoa(utils.Sum(dirList)))
+
+	var memoryNeeded = 30000000 - (70000000 - findAllDirsWithCondition(&root, -1, "none"))
+
+	dirList = []int{}
+	findAllDirsWithCondition(&root, memoryNeeded, "min")
+	fmt.Println("Day 7 Part 2 solution: " + strconv.Itoa(utils.Min(dirList)))
+}
--- utils/utils.go
+++ utils/utils.go
@@ -24,10 +24,9 @@ func StringToInt(s string) int {
 	return integer
 }
 
-/*
-*  Convenience functions for num array manipulation that I suspect I will be
-*  needing a lot
- */
+// Convenience functions for num array manipulation that I suspect I will be
+// needing a lot
+
 func Max(arr []int) int {
 	var max = 0
 	for _, e := range arr {
@@ -66,19 +65,49 @@ func Count(arr []int, v int) int {
 	return count
 }
 
+// String handling stuff
+
 // Return all common characters shared between two strings
 func Commonalities(arr1 string, arr2 string) string {
 	output := ""
 	for _, e1 := range arr1 {
 		for _, e2 := range arr2 {
 			if e1 == e2 {
-				if strings.ContainsRune(output, e1) {
-					// Do nothing
-				} else {
+				if !strings.ContainsRune(output, e1) {
 					output = output + string(e1)
 				}
 			}
 		}
 	}
 	return output
+}
+
+func StartsWith(str string, substr string) bool {
+	if len(str) >= len(substr) {
+		if str[:len(substr)] == substr {
+			return true
+		}
+	}
+	return false
+}
+
+func Remainder(str string, substr string) string {
+	var ix = 0
+	var l = len(substr)
+	var found bool = false
+
+	for !found && ix < len(str) {
+		if str[ix:ix+l] == substr {
+			found = true
+		} else {
+			ix++
+		}
+	}
+
+	if found {
+		var newStr string = str[:ix] + str[ix+l:]
+		return newStr
+	} else {
+		return str
+	}
 }
