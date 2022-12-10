a4fafae0e0bcb267d3b1f9ab58c4c1df583eb34b
halbu <fstream.c@gmail.com>
2022-12-10 06:31:47 +0000

    Day 10 part 2

--- day10/day10.go
+++ day10/day10.go
@@ -10,30 +10,48 @@ import (
 func main() {
 	var data = utils.GetData("./day10/day10-input")
 
-	buffer := [][]int{} // element format: [ value to add, cycles required for execution ]
+	stack := [][]int{} // element format: [ value to add, cycles required for execution ]
+	crt := []string{"", "", "", "", "", ""}
 
 	for _, e := range data {
 		if strings.Split(e, " ")[0] != "noop" {
-			buffer = append(buffer, []int{utils.StringToInt(strings.Split(e, " ")[1]), 2})
+			stack = append(stack, []int{utils.StringToInt(strings.Split(e, " ")[1]), 2})
 		} else {
-			buffer = append(buffer, []int{0, 1})
+			stack = append(stack, []int{0, 1})
 		}
 	}
 
-	cycle, register, total := 1, 1, 0
+	cycle, register, total, x, y := 1, 1, 0, 0, 0
 
-	for len(buffer) > 0 {
+	for len(stack) > 0 {
+		if register >= x-1 && register <= x+1 {
+			crt[y] = crt[y] + "█"
+		} else {
+			crt[y] = crt[y] + " "
+		}
+
 		if (cycle-20)%40 == 0 {
 			total += (cycle * register)
 		}
 
-		buffer[0][1]--
-		if buffer[0][1] == 0 {
-			register += buffer[0][0]
-			buffer = buffer[1:]
+		stack[0][1]--
+		if stack[0][1] == 0 {
+			register += stack[0][0]
+			stack = stack[1:]
 		}
 		cycle++
+
+		x++
+		if x > 39 {
+			x -= 40
+			y++
+		}
 	}
 
 	fmt.Println("Day 10 Part 1 solution: " + strconv.Itoa(total))
+
+	fmt.Println("Day 10 Part 2 solution: ")
+	for i := 0; i < 6; i++ {
+		fmt.Println(crt[i])
+	}
 }
